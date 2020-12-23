8d1a985a15c45c59392680796b071d86489a3ca1
Pat Monette <pat@talkswithtandra.com>
2020-12-23 19:08:10 +0000

    acf update

--- functions.php
+++ functions.php
@@ -38,12 +38,43 @@ if( function_exists('acf_add_options_page') ) {
     acf_add_options_page('Site Options');
 
     acf_add_options_page( array(
+        'page_title' => 'Top Header',
+        'capability' => 'edit_posts',
+        'icon_url' => 'dashicons-table-row-before', 
+        'position' => 5
+    ) );
+
+    acf_add_options_page( array(
         'page_title' => 'Specials',
         'capability' => 'edit_posts',
         'icon_url' => 'dashicons-star-filled', 
-        'position' => 10
+        'position' => 6
     ) );
+
+    acf_add_options_page( array(
+        'page_title' => 'Contact Info',
+        'capability' => 'edit_posts',
+        'icon_url' => 'dashicons-info-outline', 
+        'position' => 7
+    ) );
+
+    acf_add_options_page( array(
+        'page_title' => 'Hours',
+        'capability' => 'edit_posts',
+        'icon_url' => 'dashicons-clock', 
+        'position' => 8
+    ) );
+
+    acf_add_options_page( array(
+        'page_title' => 'Social Media',
+        'capability' => 'edit_posts',
+        'icon_url' => 'dashicons-facebook', 
+        'position' => 9
+    ) );
+
+    
 }
+
 
 // Custom sizes
 add_image_size( 'special', 464, 464, true );
