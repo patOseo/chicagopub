7f23d0c99187f1dd0aecdb73b4ba50363cd0e657
Pat Monette <pat@talkswithtandra.com>
2020-12-23 19:22:34 +0000

    remove site options acf

--- functions.php
+++ functions.php
@@ -35,7 +35,6 @@ add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
 
 
 if( function_exists('acf_add_options_page') ) {
-    acf_add_options_page('Site Options');
 
     acf_add_options_page( array(
         'page_title' => 'Top Header',
